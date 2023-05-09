b1cf646ce589b0b1d2702e85be81c32698e59e5a
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-05-10 02:29:23 +0530

    added details to Songcards

--- Music-Hub/src/components/InPlaylist.jsx
+++ Music-Hub/src/components/InPlaylist.jsx
@@ -6,14 +6,16 @@ export default function InPlaylist() {
     // const token = useSelector(state => state.token.value)
     const hash_token = useSelector(state => state.hash_token.value)
     const playlist_id = useSelector(state => state.playlist_id.value)
-    const [playist, set_playlist] = useState([])
+    const [playlist, set_playlist] = useState([])
     // console.log(playlist_id)
 
+    
 
     useEffect(()=>{
         if (playlist_id != undefined) {
             get_playlist()
         }
+
         
     }, [playlist_id])
     const get_playlist = async() => {
@@ -26,14 +28,12 @@ export default function InPlaylist() {
         })
         let parsed_data = await result.json()
         set_playlist(parsed_data.tracks.items)
-        // console.log(parsed_data)
-        // console.log(token)
     }
 
     
 
     return (
-        <div className='h-full w-[100vw] bg-gradient-to-b from-[#4705a4d6] to-[#0000005d] pl-[16.66%]'>
+        <div className='h-fit w-[100vw] bg-gradient-to-b from-[#4705a4d6] to-[#0000005d] pl-[16.66%]'>
             <div className=' px-8 w-full flex items-end py-16'>
                 <img src="https://th.bing.com/th/id/OIP.o9A22FDuVlB74qPIZfX3RgHaHa?w=178&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='h-60' alt="" />
                 <div className='flex flex-col pb-5 justify-center px-4'>
@@ -46,9 +46,9 @@ export default function InPlaylist() {
                 </div>
             </div>
             <div className='bg-white h-[1px] mt-10 mb-2' />
-            {playist ? <div className='py-4'>
-                {playist.map((element)=>{
-                    return <Song_Cards/>
+            {playlist ? <div className='py-4'>
+                {playlist.map((element)=>{
+                    return <Song_Cards artist = {element.track.artist ? element.track.artist[0].name : 'Unavailable'} name = {element.track.name ? element.track.name : 'Unavailable'} img={element.track.album.images[1] ? element.track.album.images[1].url : ""}/>
                 })}
                 </div> : <div className='py-4'></div>}
             {/* <div className='py-4'>
--- Music-Hub/src/components/Song_Cards.jsx
+++ Music-Hub/src/components/Song_Cards.jsx
@@ -3,17 +3,17 @@ import React, { Component } from 'react'
 export default class Song_Cards extends Component {
   render() {
     return (
-      <div className='bg-black w-full rounded-lg h-14 flex items-center'>
+      <div className='bg-black w-full py-3 rounded-lg h-14 flex items-center'>
         <div className='w-[30%] flex items-center px-[9px]'>
           <h3 className='px-1 text-white'>1</h3>
-          <img src="https://th.bing.com/th/id/OIP.o9A22FDuVlB74qPIZfX3RgHaHa?w=178&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='h-10 px-[9px]' alt="" />
+          <img src={this.props.img} className='h-10 px-[9px]' alt="" />
           <div className='px-[9px]'>
-            <h2 className='text-gray-200 text-[17px]'>Blinding Lights</h2>
-            <p className='text-gray-400 text-[15px]'>Weeknd</p>
+            <h2 className='text-gray-200 text-[17px]'>{this.props.name}</h2>
+            <p className='text-gray-400 text-[15px]'>{this.props.artist}</p>
           </div>
         </div>
         <div className='flex items-center'>
-          <h3 className='text-gray-400'>Blinding Lights</h3>
+          <h3 className='text-gray-400'></h3>
         </div>
         <span className="material-symbols-outlined pl-28 cursor-pointer" style={{color: 'white'}} >
           favorite
