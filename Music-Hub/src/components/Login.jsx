91fbe9a661443a3d8c36038fe56695fb44fed17b
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-05-25 08:41:46 +0530

    added the spotify img

--- Music-Hub/src/components/Login.jsx
+++ Music-Hub/src/components/Login.jsx
@@ -6,7 +6,7 @@ function Login(props) {
   return (
 
     <div className='h-[100vh] bg-green-500 flex justify-center items-center flex-col'>
-      <img className='w-[75vw]' src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="" />
+      <img className='w-[75vw]' src="src\assets\Spotify_Logo.png" alt="" />
       <Link to='Connect'>
         <button className='text-green-400 text-center bg-black text-3xl h-16 w-72 rounded-2xl'>Connect To Spoitfy</button>
       </Link>
